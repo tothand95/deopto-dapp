1c154dcdec0c7d315a8dff850b5293968982efd5
Tóth András <tothand95@gmail.com>
2022-12-21 16:00:39 +0100

    working demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Web3Auth } from "@web3auth/modal";
 import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
 import RPC from "./web3rpc";
@@ -7,34 +7,34 @@ const clientId = "YOUR_WEB3AUTH_CLIENT_ID"; // get from https://dashboard.web3au
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-    title = "angular-app";
-    web3auth: Web3Auth | null = null;
-    provider: SafeEventEmitterProvider | null = null;
-    isModalLoaded = false;
+export class AppComponent implements OnInit {
+  title = "angular-app";
+  web3auth: Web3Auth | null = null;
+  provider: SafeEventEmitterProvider | null = null;
+  isModalLoaded = false;
 
-    async ngOnInit() {
-      this.web3auth = new Web3Auth({
-        clientId,
-        chainConfig: {
-          chainNamespace: CHAIN_NAMESPACES.EIP155,
-          chainId: "0x38",
-          rpcTarget: "https://bsc-dataseed.binance.org/", // This is the public RPC we have added, please pass on your own endpoint while creating an app
-        },
-      });
-      const web3auth = this.web3auth;
+  async ngOnInit() {
+    this.web3auth = new Web3Auth({
+      clientId,
+      chainConfig: {
+        chainNamespace: CHAIN_NAMESPACES.EIP155,
+        chainId: "0x38",
+        rpcTarget: "https://bsc-dataseed.binance.org/", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+      },
+    });
+    const web3auth = this.web3auth;
 
 
-      await web3auth.initModal();
-      if (web3auth.provider) {
-        this.provider = web3auth.provider;
-      }
-      this.isModalLoaded = true;
+    await web3auth.initModal();
+    if (web3auth.provider) {
+      this.provider = web3auth.provider;
     }
+    this.isModalLoaded = true;
+  }
 
-    login = async () => {
+  async login() {
     if (!this.web3auth) {
       console.log("web3auth not initialized yet");
       return;
@@ -42,83 +42,84 @@ export class AppComponent {
     const web3auth = this.web3auth;
     this.provider = await web3auth.connect();
     console.log("logged in");
-    };
+  };
 
-    getUserInfo = async () => {
-      if (!this.web3auth) {
-        console.log("web3auth not initialized yet");
-        return;
-      }
-      const user = await this.web3auth.getUserInfo();
-      console.log(user);
-    };
+  async getUserInfo() {
+    if (!this.web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    const user = await this.web3auth.getUserInfo();
+    console.log(user);
+  };
 
-    logout = async () => {
-      if (!this.web3auth) {
-        console.log("web3auth not initialized yet");
-        return;
-      }
-      await this.web3auth.logout();
-      this.provider = null;
-      console.log("logged out");
-    };
+  async logout() {
+    if (!this.web3auth) {
+      console.log("web3auth not initialized yet");
+      return;
+    }
+    await this.web3auth.logout();
+    this.provider = null;
+    console.log("logged out");
+  };
 
-    getChainId = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const chainId = await rpc.getChainId();
-      console.log(chainId);
-    };
-    getAccounts = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const address = await rpc.getAccounts();
-      console.log(address);
-    };
+  async getChainId() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const chainId = await rpc.getChainId();
+    console.log(chainId);
+  };
 
-    getBalance = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const balance = await rpc.getBalance();
-      console.log(balance);
-    };
+  async getAccounts() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const address = await rpc.getAccounts();
+    console.log(address);
+  };
 
-    sendTransaction = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const receipt = await rpc.sendTransaction();
-      console.log(receipt);
-    };
+  async getBalance() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const balance = await rpc.getBalance();
+    console.log(balance);
+  };
 
-    signMessage = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const signedMessage = await rpc.signMessage();
-      console.log(signedMessage);
-    };
+  async sendTransaction() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const receipt = await rpc.sendTransaction();
+    console.log(receipt);
+  };
 
-    getPrivateKey = async () => {
-      if (!this.provider) {
-        console.log("provider not initialized yet");
-        return;
-      }
-      const rpc = new RPC(this.provider);
-      const privateKey = await rpc.getPrivateKey();
-      console.log(privateKey);
-    };
+  async signMessage() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const signedMessage = await rpc.signMessage();
+    console.log(signedMessage);
+  };
+
+  async getPrivateKey() {
+    if (!this.provider) {
+      console.log("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(this.provider);
+    const privateKey = await rpc.getPrivateKey();
+    console.log(privateKey);
+  };
 }
