5dcfdf8ad47a3e118783ad31f6146e010b275d01
Tóth András <tothand95@gmail.com>
2022-12-22 18:43:38 +0100

    react directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RenderReactDirective } from './directives/render-react.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RenderReactDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/render-react.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, inject, Input } from '@angular/core';
+import { ElementType, ComponentProps, createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+
+@Directive({
+  selector: '[appRenderReact]'
+})
+export class RenderReactDirective<Comp extends ElementType> {
+  @Input() reactComponent: Comp;
+  @Input() props: ComponentProps<Comp>;
+
+  private root = createRoot(inject(ElementRef).nativeElement)
+
+  ngOnChanges() {
+    this.root.render(createElement(this.reactComponent, this.props))
+  }
+
+  ngOnDestroy() {
+    this.root.unmount();
+  }
+
+}
