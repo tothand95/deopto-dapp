a249caab037f85d5485d1684dd04eaa8c314d884
Tóth András <tothand95@gmail.com>
2022-12-28 21:58:15 +0100

    app comp with navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RoadmapComponent } from './components/roadmap/roadmap.component';
 import { VoteComponent } from './components/vote/vote.component';
+import { VotingProcedureComponent } from './components/voting-procedure/voting-procedure.component';
+import { WhitepaperComponent } from './components/whitepaper/whitepaper.component';
 
 const routes: Routes = [
-  {
-    path: '', component: VoteComponent
-  }
+  { path: '', component: HomeComponent },
+  { path: 'vote', component: VoteComponent },
+  { path: 'whitepaper', component: WhitepaperComponent },
+  { path: 'voting-procedure', component: VotingProcedureComponent },
+  { path: 'roadmap', component: RoadmapComponent }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deopto-dapp';
+  onTop = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(_event: Event) {
+    this.onTop = window.scrollY === 0;
+  }
 }
