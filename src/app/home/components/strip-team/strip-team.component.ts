af2149cde7a1eaa90552b7f296224b980948be37
Tóth András <tothand95@gmail.com>
2023-01-03 22:55:23 +0100

    launch strip

---
+++ src/app/home/components/strip-team/strip-team.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-strip-team',
+  templateUrl: './strip-team.component.html',
+  styleUrls: ['./strip-team.component.scss']
+})
+export class StripTeamComponent {
+
+}
